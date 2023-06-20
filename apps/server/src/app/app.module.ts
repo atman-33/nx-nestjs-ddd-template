c0f1db52410799aee4598e9f591c59130d2a89fe
atman-33 <gpbjk0304@gmail.com>
2023-06-20 21:00:37 +0900

    add config and joi

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RegionsModule } from './regions/regions.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['./apps/server/src/.env'],
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+        ORACLE_USER: Joi.string().required(),
+        ORACLE_PASSWORD: Joi.string().required(),
+        ORACLE_DATA_SOURCE: Joi.string().required(),
+      }),
+    }),
+    RegionsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
